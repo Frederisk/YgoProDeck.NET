b90ed78ef15c6a8dd486210df17944a6b297384e
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-06-09 02:05:33 +0800

    Update Program.cs

--- YgoProDeck.Telegram/Program.cs
+++ YgoProDeck.Telegram/Program.cs
@@ -53,6 +53,8 @@ namespace YgoProDeck.Telegram {
                 await (update.Type switch {
                     UpdateType.InlineQuery => BotOnInlineQueryReceived(bot, update.InlineQuery!), // Query
                     UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(bot, update.ChosenInlineResult!), // Feedback
+                    // UpdateType.ChatMember => Logging(bot, $"ChatMember: {update.ChatMember}"),
+                    UpdateType.Message => BotOnMessageReceived(bot, update.Message!), // Message
                     _ => Task.CompletedTask,
                 });
             } catch (Exception ex) {
@@ -173,6 +175,54 @@ namespace YgoProDeck.Telegram {
                 Chosen: `{chosenInlineResult.ResultId}`
                 Query: `{chosenInlineResult.Query}`
                 """);
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message) {
+            if (message.Type != MessageType.Text) {
+                return;
+            }
+
+            if (message.Text is null) {
+                return;
+            }
+            await botClient.SendTextMessageAsync(
+                chatId: message.Chat,
+                text: """
+                Hello, I'm @InYgoProDeckBot!
+                I can help you to search for Yu-Gi-Oh! cards.
+
+                Currently, I can only search card in inline mode. You only need to write the card name in your input box like this:
+
+                `@InYgoProDeckBot Maxx "C"`
+
+                I will show you the details of the card.
+
+                For more information, you can visit <https://github.com/Frederisk/YgoProDeck.NET>
+                """,
+                parseMode: ParseMode.Markdown
+                );
+
+            // if (message.Text.StartsWith("/start")) {
+            //     await botClient.SendTextMessageAsync(
+            //         chatId: message.Chat,
+            //         text: "Hello, I'm YgoProDeckBot. I can help you to search for Yu-Gi-Oh! cards. Just type the card name and I will show you the details.");
+            //     return;
+            // }
+
+            // if (message.Text.StartsWith("/help")) {
+            //     await botClient.SendTextMessageAsync(
+            //         chatId: message.Chat,
+            //         text: "Just type the card name and I will show you the details.");
+            //     return;
+            // }
+
+            // if (message.Text.StartsWith("/")) {
+            //     await botClient.SendTextMessageAsync(
+            //         chatId: message.Chat,
+            //         text: "Unknown command. Type /help for help."
+            //         );
+            //     return;
+            // }
         }
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
